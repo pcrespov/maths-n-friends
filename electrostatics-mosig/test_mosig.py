4cec228c9b05d8433f29fed25b1b5f9679ace1b2
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-08-11 16:30:50 +0200

    avg discr GF

--- electrostatics-mosig/test_mosig.py
+++ electrostatics-mosig/test_mosig.py
@@ -1,11 +1,16 @@
 from typing import Callable
 
-
-def definite_integral(primitive: Callable, x1: float, x2: float, y1: float, y2: float):
-    return primitive(x1, y1) + primitive(x2, y2) - primitive(x1, y2) - primitive(x2, y1)
-
-
 import numpy as np
+
+
+def definite_integral(primitive: Callable, x1, x2, y1, y2, h):
+    return (
+        primitive(x1, y1, h)
+        + primitive(x2, y2, h)
+        - primitive(x1, y2, h)
+        - primitive(x2, y1, h)
+    )
+
 
 #
 # Primitives of Table I
@@ -35,7 +40,7 @@ def primitive_of_vg(u, v, h):
 
 
 def primitive_of_uvg(u, v, h):
-    return ((u**2 + v**2 + h**2) ** (3 / 2)) / 3.
+    return ((u**2 + v**2 + h**2) ** (3 / 2)) / 3.0
 
 
 def discrete_green_function(xc, yc, zc, a, b):
@@ -47,8 +52,43 @@ def discrete_green_function(xc, yc, zc, a, b):
         - primitive_of_g(u=u1, v=v2, h=zc)
         - primitive_of_g(u=u2, v=v1, h=zc)
     )
+    assert result == definite_integral(primitive_of_g, u1, u2, v1, v2, zc)
     S = a * b
     return result / S
 
 
 def average_discrete_green_function(xc, yc, zc, a, b):
+    x1, x2, y1, y2 = xc - a, xc, yc - b, yc
+    Yuu = (
+        x1 * y1 * definite_integral(primitive_of_g, x1, x2, y1, y2, zc)
+        - y1 * definite_integral(primitive_of_ug, x1, x2, y1, y2, zc)
+        - x1 * definite_integral(primitive_of_vg, x1, x2, y1, y2, zc)
+        + definite_integral(primitive_of_uvg, x1, x2, y1, y2, zc)
+    )
+
+    x1, x2, y1, y2 = xc - a, xc, yc, yc + b
+    Yud = (
+        -x1 * y2 * definite_integral(primitive_of_g, x1, x2, y1, y2, zc)
+        + y2 * definite_integral(primitive_of_ug, x1, x2, y1, y2, zc)
+        + x1 * definite_integral(primitive_of_vg, x1, x2, y1, y2, zc)
+        - definite_integral(primitive_of_uvg, x1, x2, y1, y2, zc)
+    )
+
+    x1, x2, y1, y2 = xc, xc + a, yc - b, yc
+    Ydu = (
+        -x2 * y1 * definite_integral(primitive_of_g, x1, x2, y1, y2, zc)
+        + y1 * definite_integral(primitive_of_ug, x1, x2, y1, y2, zc)
+        + x2 * definite_integral(primitive_of_vg, x1, x2, y1, y2, zc)
+        - definite_integral(primitive_of_uvg, x1, x2, y1, y2, zc)
+    )
+
+    x1, x2, y1, y2 = xc, xc + a, yc, yc + b
+    Ydd = (
+        x2 * y2 * definite_integral(primitive_of_g, x1, x2, y1, y2, zc)
+        - y2 * definite_integral(primitive_of_ug, x1, x2, y1, y2, zc)
+        - x2 * definite_integral(primitive_of_vg, x1, x2, y1, y2, zc)
+        + definite_integral(primitive_of_uvg, x1, x2, y1, y2, zc)
+    )
+
+    S = a * b
+    return (Yuu + Yud + Ydu + Ydd) / S**2
