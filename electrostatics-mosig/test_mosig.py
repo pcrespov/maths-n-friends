4fb07eb74abbabe079533f3bbe6d64c92feafbff
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-08-12 14:34:19 +0200

    adds mom

--- electrostatics-mosig/test_mosig.py
+++ electrostatics-mosig/test_mosig.py
@@ -1,6 +1,9 @@
+from hmac import new
 from typing import Callable
+from scipy.constants import epsilon_0
 
 import numpy as np
+import pytest
 
 #
 # Primitives of Table I
@@ -103,9 +106,6 @@ def average_discrete_green_function(xc, yc, zc, a, b):
     return (Yuu + Yud + Ydu + Ydd) / S**2
 
 
-import pytest
-
-
 @pytest.param(
     "gal,pm,gf,loc",
     [
@@ -120,9 +120,118 @@ import pytest
 )
 def test_integration_values_table(gal, pm, gf, loc):
     assert discrete_green_function(*loc, a=1, b=1) == pytest.approx(pm, abs=0.0001)
+
     assert average_discrete_green_function(*loc, a=1, b=1) == pytest.approx(
         gal, abs=0.0001
     )
+    
     assert 1 / np.sqrt(loc[0] ** 2 + loc[1] ** 2 + loc[3] ** 2) == pytest.approx(
         gf, abs=0.001
     )
+
+
+def eval_stat_mom(
+    x_basis,
+    y_basis,
+    z_basis,
+    x_test,
+    y_test,
+    z_test,
+    *,
+    a: float,
+    b: float,
+    factor_resolution_h: float,
+    use_galerkin: bool
+):
+    xc = np.abs(x_basis - x_test)
+    yc = np.abs(y_basis - y_test)
+    zc = np.abs(z_basis - z_test)
+    rh = np.sqrt(xc**2 + yc**2)
+
+    use_gf_values = rh > factor_resolution_h * max(a, b)
+    if use_gf_values:
+        r = np.sqrt(xc**2 + yc**2 + zc**2)  # distance between cell centers
+        return 1 / r
+
+    if use_galerkin:
+        return average_discrete_green_function(xc, yc, zc, a, b)
+
+    return discrete_green_function(xc, yc, zc, a, b)
+
+
+def eval_excitation_constant_v():
+    
+
+def run_static_plate(
+    a_side: float = 1.0, b_side: float = 1.0, m: int = 20, n: int = 20
+):
+    a = a_side / m
+    b = b_side / n
+
+    x = np.linspace(a / 2, a_side - a / 2, m)
+    y = np.linspace(b / 2, b_side - b / 2, n)
+
+    x_matrix, y_matrix = np.meshgrid(x, y)
+
+    x_vector = np.reshape(x_matrix, newshape=(1, m * n))
+    y_vector = np.reshape(y_matrix, newshape=(1, m * n))
+
+    # filling the Mom matrix
+    ndim = m * n
+    tresh = 10
+    mom = np.zeros((ndim, ndim))
+    for i in range(ndim):
+        for j in range(ndim):
+            mom[i, j] = eval_stat_mom(
+                x_basis=x_vector[j],
+                y_basis=y_vector[j],
+                z_basis=0,
+                x_test=x_vector[i],
+                y_test=x_vector[i],
+                z_test=0,
+                a=a,
+                b=b,
+                factor_resolution_h=tresh,
+                use_galerkin=False,
+            )
+
+    # excitation vector
+
+    # (1) constant potential
+    def _excite_with_constant_potential(v):
+        return np.full(shape=x_vector.shape, fill_value=v)
+
+    # (2) a point charge
+    def _excite_with_point_charge():
+        excv = np.zeros(x_vector.shape)
+        xq, yq, zq = 0.5, 0.5, 0.5
+        for i in range(excv.size):
+            excv[i] = 1.0 / np.sqrt(
+                (xq - x_vector[i]) ** 2 + (yq - y_vector[i]) ** 2 + zq**2
+            )
+        return excv
+    
+    excitation_potential = 1
+    excv = _excite_with_constant_potential(excitation_potential)
+
+
+    # Solving linear system
+    charge = np.linalg.solve(mom, excv)
+
+    # Total charge of the plate
+    total_charge = charge.sum()
+
+    # If constant potential, then the normalized capacity is
+    normalized_capacity = total_charge / excitation_potential
+ 
+    capacity = 4. * np.pi* epsilon_0 # Farads
+
+    print(f"{total_charge=} Coulombs")
+    print(f"{normalized_capacity=}")
+    print(f"{capacity=} F")
+
+
+
+
+
+
