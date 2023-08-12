91335159a4f005469210d1272d163094cc5b1366
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2023-08-12 15:24:35 +0200

    cleanup

--- electrostatics-mosig/test_mosig.py
+++ electrostatics-mosig/test_mosig.py
@@ -1,9 +1,8 @@
-from hmac import new
 from typing import Callable
-from scipy.constants import epsilon_0
 
 import numpy as np
 import pytest
+from scipy.constants import epsilon_0
 
 #
 # Primitives of Table I
@@ -106,25 +105,33 @@ def average_discrete_green_function(xc, yc, zc, a, b):
     return (Yuu + Yud + Ydu + Ydd) / S**2
 
 
-@pytest.param(
-    "gal,pm,gf,loc",
-    [
-        (2.9732, 3.5255, np.Inf, (0, 0, 0)),
-        (1.1121, 1.0380, 1.0, (1, 0, 0)),
-        (0.7490, 0.7247, 0.7071, (1, 1, 0)),
-        (0.2513, 0.2506, 0.2500, (4, 0, 0)),
-        (2.4674, 2.9533, 10.0, (0, 0, 0.1)),
-        (0.8788, 0.9286, 1.0, (0, 0, 1)),
-        (0.1987, 0.1933, 0.2, (0, 0, 5)),
-    ],
-)
-def test_integration_values_table(gal, pm, gf, loc):
+# gal,pm,gf,loc
+GAL, PM, GF, LOC = 0, 1, 2, 3
+_reference_table = [
+    (2.9732, 3.5255, np.Inf, (0, 0, 0)),
+    (1.1121, 1.0380, 1.0, (1, 0, 0)),
+    (0.7490, 0.7247, 0.7071, (1, 1, 0)),
+    (0.2513, 0.2506, 0.2500, (4, 0, 0)),
+    (2.4674, 2.9533, 10.0, (0, 0, 0.1)),
+    (0.8788, 0.9286, 1.0, (0, 0, 1)),
+    (0.1987, 0.1933, 0.2, (0, 0, 5)),
+]
+
+
+@pytest.param("pm,loc", [(row[PM], row[LOC]) for row in _reference_table])
+def test_discrete_green_function(pm, loc):
     assert discrete_green_function(*loc, a=1, b=1) == pytest.approx(pm, abs=0.0001)
 
+
+@pytest.param("gal,loc", [(row[GAL], row[LOC]) for row in _reference_table])
+def test_average_discrete_green_function(gal, loc):
     assert average_discrete_green_function(*loc, a=1, b=1) == pytest.approx(
         gal, abs=0.0001
     )
-    
+
+
+@pytest.param("gal,loc", [(row[GF], row[LOC]) for row in _reference_table])
+def test_green_function(gf, loc):
     assert 1 / np.sqrt(loc[0] ** 2 + loc[1] ** 2 + loc[3] ** 2) == pytest.approx(
         gf, abs=0.001
     )
@@ -141,7 +148,7 @@ def eval_stat_mom(
     a: float,
     b: float,
     factor_resolution_h: float,
-    use_galerkin: bool
+    use_galerkin: bool,
 ):
     xc = np.abs(x_basis - x_test)
     yc = np.abs(y_basis - y_test)
@@ -158,9 +165,6 @@ def eval_stat_mom(
 
     return discrete_green_function(xc, yc, zc, a, b)
 
-
-def eval_excitation_constant_v():
-    
 
 def run_static_plate(
     a_side: float = 1.0, b_side: float = 1.0, m: int = 20, n: int = 20
@@ -210,10 +214,9 @@ def run_static_plate(
                 (xq - x_vector[i]) ** 2 + (yq - y_vector[i]) ** 2 + zq**2
             )
         return excv
-    
+
     excitation_potential = 1
     excv = _excite_with_constant_potential(excitation_potential)
-
 
     # Solving linear system
     charge = np.linalg.solve(mom, excv)
@@ -223,15 +226,9 @@ def run_static_plate(
 
     # If constant potential, then the normalized capacity is
     normalized_capacity = total_charge / excitation_potential
- 
-    capacity = 4. * np.pi* epsilon_0 # Farads
+
+    capacity = 4.0 * np.pi * epsilon_0  # Farads
 
     print(f"{total_charge=} Coulombs")
     print(f"{normalized_capacity=}")
     print(f"{capacity=} F")
-
-
-
-
-
-
